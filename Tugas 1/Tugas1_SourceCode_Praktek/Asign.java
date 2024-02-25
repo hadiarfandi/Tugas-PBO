c5d3071be0aae3b5d79f0cdc8e4b4f1b9b58e297
hadiarfandi <161113820+hadiarfandi@users.noreply.github.com>
2024-02-25 20:36:17 +0800

    Add files via upload

---
+++ Tugas 1/Tugas1_SourceCode_Praktek/Asign.java
@@ -0,0 +1,19 @@
+/*
+Nim	:13020220150
+Nama	:Arfandi Hadi Kusuma
+Minggu, 25 Februari 2024
+*/ 
+
+public class Asign {
+     /**
+     * @param args
+     */
+     public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	/* Kamus */
+	int i;	
+	/* Program */
+		System.out.print ("hello\n"); i = 5;
+		System.out.println ("Ini nilai i : " + i);
+	}
+}
